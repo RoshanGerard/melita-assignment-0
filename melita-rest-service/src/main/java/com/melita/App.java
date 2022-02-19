41df64755e4f8f788be6fcc0e6bd809bfce68d19
Roshan G. Bolonna <srgbolonna@gmail.com>
2022-02-19 14:45:37 +0530

    ****** initial commit. ******

---
+++ melita-rest-service/src/main/java/com/melita/App.java
@@ -0,0 +1,17 @@
+package com.melita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Run as spring boot application.
+ *
+ * @author Roshan Bolonna
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
